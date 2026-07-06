78a94c075d711a36ab8070a9708e9e24a5e83baa
Fatima Notron <fatimanotron@gmail.com>
2026-07-06 15:29:38 +0330

    add editline: move around inputs

--- mylisp.c
+++ mylisp.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 int main(int argc, char** argv){
 
@@ -8,9 +11,13 @@ int main(int argc, char** argv){
     puts("Press Ctrl+c to Exit\n");
 
     while(1){
-        fputs("Lispy>", stdout);
-        fgets(input,2048, stdin);
-        printf("No you're a %s", input);
+
+        char* input = readline("lispy>");
+        add_history(input);
+        printf("No you're a %s\n", input);
+
+        free(input);
+
     }
     return 0;
 }
